96ae5bea5a8ca750bcbe0431c758cae5b3621ea7
Александр Могильный <blagimi@yandex.ru>
2017-04-10 02:08:44 +0500

    0.0.1

--- Ems/MainForm/MainWindow.xaml.cs
+++ Ems/MainForm/MainWindow.xaml.cs
@@ -1,17 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+using MainForm.ViewModel;
 
 namespace MainForm
 {
@@ -22,7 +16,57 @@ namespace MainForm
     {
         public MainWindow()
         {
+            WindowStartupLocation = System.Windows.WindowStartupLocation.CenterScreen;
             InitializeComponent();
+        }
+
+        private void StackPanel_Loaded(object sender, RoutedEventArgs e)
+        {
+            // MainFrame.NavigationService.Navigate(new Uri("Pages/Welcome.xaml",UriKind.Relative));
+        }
+
+        string cn = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\blagi\Source\Repos\Ems\Ems\MainForm\ApplicationData.mdf;Integrated Security = True";
+        DataTable userTable;
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            userTable = new DataTable();
+            if (LoginBox.Text.Length < 1 || PasswordBox.Password.Length < 1)
+            {
+                MessageBox.Show("Значения Логин|Пароль не могут быть пустыми.", "Ошибка при запуске приложения.");
+                return;
+            }
+            try
+            {
+                SqlConnection connection = new SqlConnection(cn);
+                SqlCommand command = new SqlCommand("Select * from tbl_login where UserName=@username and Password=@password", connection);
+                command.Parameters.AddWithValue("@username", LoginBox.Text);
+                command.Parameters.AddWithValue("@password", PasswordBox.Password);
+                connection.Open();
+                SqlDataAdapter adapt = new SqlDataAdapter(command);
+                DataSet dataset = new DataSet();
+                adapt.Fill(dataset);
+                connection.Close();
+
+                int count = dataset.Tables[0].Rows.Count;
+                if (count == 1)
+                {
+                    
+                    userTitle.Text = LoginBox.Text;
+                    MainFrame.NavigationService.Navigate(new Uri("Pages/SecondOne.xaml", UriKind.Relative));
+                    MessageBox.Show("Succses");
+
+
+                }
+                else
+                {
+                    MessageBox.Show("\n Пользователь с указной парой Логин|Пароль не найден. \n Возможно вы неверно ввели пароль.", "Ошибка при авторизации пользователя.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Отсутсвует подключение к базе данных, обратитесь к администратору \n {ex.Message}", "Ошибка при авторизации пользователя.");
+            }
         }
     }
 }
---
+++ Ems/MainForm/Pages/Welcome.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Data.SqlClient;
+using System.Data;
+using System;
+using System.Configuration;
+using MainForm.ViewModel;
+
+namespace MainForm.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Welcome.xaml
+    /// </summary>
+    public partial class Welcome : Page
+    {
+        public Welcome()
+        {
+            InitializeComponent();
+        }
+
+        string cn = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\blagi\Source\Repos\Ems\Ems\MainForm\ApplicationData.mdf;Integrated Security = True";
+        DataTable userTable;
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            userTable = new DataTable();
+            if (LoginBox.Text.Length <1 || PasswordBox.Password.Length <1)
+            {
+                MessageBox.Show("Значения Логин|Пароль не могут быть пустыми.","Ошибка при запуске приложения.");
+                return;
+            }
+            try
+            {
+                SqlConnection connection = new SqlConnection(cn);
+                SqlCommand command = new SqlCommand("Select * from tbl_login where UserName=@username and Password=@password", connection);
+                command.Parameters.AddWithValue("@username", LoginBox.Text);
+                command.Parameters.AddWithValue("@password", PasswordBox.Password); 
+                connection.Open();
+                SqlDataAdapter adapt = new SqlDataAdapter(command);
+                DataSet dataset = new DataSet();
+                adapt.Fill(dataset);
+                connection.Close();
+                
+                int count = dataset.Tables[0].Rows.Count;
+                if (count == 1)
+                {
+                    NavigationService.Navigate(new Uri("Pages/SecondOne.xaml", UriKind.Relative));
+                    MessageBox.Show("Succses");
+
+
+                }
+                else
+                {
+                    MessageBox.Show("\n Пользователь с указной парой Логин|Пароль не найден. \n Возможно вы неверно ввели пароль.","Ошибка при авторизации пользователя.");
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show($"Отсутсвует подключение к базе данных, обратитесь к администратору \n {ex.Message}","Ошибка при авторизации пользователя.");
+            }
+
+
+        }
+    }
+}
